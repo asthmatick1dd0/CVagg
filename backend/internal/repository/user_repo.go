bd96cb8d8b054b9bc6e73feb042dc5d8fe04c5e1
Kekzuke <artem.golub2019@gmail.com>
2025-10-31 15:57:04 +0300

    Update update function in user repo

--- backend/internal/repository/user_repo.go
+++ backend/internal/repository/user_repo.go
@@ -1,10 +1,8 @@
 package repository
 
 import (
-	"gorm.io/gorm"
-
-	"github.com/asthmatick1dd0/CVagg/backend/internal/models"
 	"github.com/asthmatick1dd0/CVagg/internal/models"
+	"gorm.io/gorm"
 )
 
 type UserRepository interface {
@@ -22,13 +20,22 @@ func NewUserRepository(db *gorm.DB) UserRepository {
 	return &userRepo{db}
 }
 
-func (r *userRepo) Create(u *models.User) error {
-	return r.db.Create(u).Error
+func (r *userRepo) Create(user *models.User) error {
+	return r.db.Create(user).Error
 }
 
-func (r *userRepo) GetById(id uint) (*models.User, error) {
+func (r *userRepo) GetByID(id uint) (*models.User, error) {
 	var user models.User
 	if err := r.db.First(&user, id).Error; err != nil {
-
+		return nil, err
 	}
+	return &user, nil
+}
+
+func (r *userRepo) Update(user *models.User) error {
+	return r.db.Save(user).Error
+}
+
+func (r *userRepo) Delete(id uint) error {
+	return r.db.Where("id = ?", id).Delete(&models.User{}).Error
 }
