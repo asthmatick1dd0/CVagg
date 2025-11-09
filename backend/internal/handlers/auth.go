06536d3d16724f39c1227f692fbd29fce8eddb3a
asthmatick1d <ansergoff@gmail.com>
2025-11-10 02:03:03 +0300

    Quick fixes
    Fix incorrect name of imports in auth.go

--- backend/internal/handlers/auth.go
+++ backend/internal/handlers/auth.go
@@ -3,18 +3,18 @@ package handlers
 import (
 	"github.com/asthmatick1dd0/CVagg/internal/models"
 	"github.com/asthmatick1dd0/CVagg/internal/repository"
-	"github.com/asthmatick1dd0/CVagg/internal/transport"
-	internal "github.com/asthmatick1dd0/CVagg/internal/validation"
+	"github.com/asthmatick1dd0/CVagg/internal/transport/input"
+	validation "github.com/asthmatick1dd0/CVagg/internal/validation"
 	"github.com/gofiber/fiber/v2"
 )
 
 func SignUpHandler(c *fiber.Ctx) error {
-	var input transport.SignUpInput
+	var input input.SignUpInput
 	err := c.QueryParser(&input)
 	if err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(err.Error())
 	}
-	errs := internal.ValidateStruct(input)
+	errs := validation.ValidateStruct(input)
 	if len(errs) != 0 {
 		return c.Status(fiber.StatusBadRequest).JSON(errs)
 	}
