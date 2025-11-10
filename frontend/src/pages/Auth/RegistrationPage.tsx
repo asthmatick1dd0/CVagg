2cc49d0b636207f2f368e61988f504b351800f03
Анастасия Исаева <isaevaolga0371@gmail.com>
2025-11-10 21:58:58 +0300

    Добавлена страница регистрации

---
+++ frontend/src/pages/Auth/RegistrationPage.tsx
@@ -0,0 +1,40 @@
+import { Card, CardHeader, CardFooter, CardTitle, CardContent } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+function RegistrationPage(){
+    return (
+    <>
+    <div className="min-h-screen flex items-center justify-center custom-gradient">
+        <Card className="scale-105 md:scale-125 lg:scale-140">
+            <CardHeader className="flex flex-col justify-center items-center">
+                <CardTitle className="text-lg">Войти в аккаунт</CardTitle>
+            </CardHeader>
+            <CardContent className="">
+                <form>
+                    {/* TODO (в будущем): добавить/заменить юзернейм на почту */}
+                    <div className="py-2">
+                        <label htmlFor="uname">Имя пользователя</label>
+                        <Input id="uname" type="text" placeholder="you@example.com" />
+                    </div>
+                    <div className="">
+                        <label htmlFor="password">Пароль</label>
+                        <Input id="password" type="password" placeholder="••••••••" />
+                    </div>
+                </form>
+            </CardContent>
+            <CardFooter className="flex flex-col">
+                <Button type="submit" className="w-full">Войти</Button>
+                <Button variant="link">
+                    <a href="./pages/Auth/RegistrationPage">
+                        <i>Нет аккаунта?</i>
+                    </a>
+                </Button>
+            </CardFooter>
+        </Card>
+    </div>
+    </>
+)
+}
+
+export default RegistrationPage;
