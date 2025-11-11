2b2728b268c0957441e9d9d64c3577c119ca4f31
Анастасия Исаева <isaevaolga0371@gmail.com>
2025-11-11 12:35:39 +0300

    fix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom"
 import LandingPage from "./pages/LandingPage/LandingPage"
 import LoginPage from "./pages/Auth/LoginPage";
+import RegistrationPage from "./pages/Auth/RegistrationPage";
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage/>} />
         <Route path="/login" element={<LoginPage />} />
-        {/* TODO (Настя): после создания страницы регистрации добавить рут на неё */}
+        <Route path="/registration" element={<RegistrationPage />} />
       </Routes>
     </>
   );
--- frontend/src/pages/Auth/RegistrationPage.tsx
+++ frontend/src/pages/Auth/RegistrationPage.tsx
@@ -4,36 +4,48 @@ import { Button } from "@/components/ui/button";
 
 function RegistrationPage(){
     return (
-    <>
     <div className="min-h-screen flex items-center justify-center custom-gradient">
         <Card className="scale-105 md:scale-125 lg:scale-140">
+
             <CardHeader className="flex flex-col justify-center items-center">
-                <CardTitle className="text-lg">Войти в аккаунт</CardTitle>
+                <CardTitle className="text-lg">Регистрация</CardTitle>
             </CardHeader>
+
             <CardContent className="">
                 <form>
-                    {/* TODO (в будущем): добавить/заменить юзернейм на почту */}
                     <div className="py-2">
-                        <label htmlFor="uname">Имя пользователя</label>
-                        <Input id="uname" type="text" placeholder="you@example.com" />
+                        <label htmlFor="login">Логин</label>
+                        <Input id="login" type="text" placeholder="Irina332217" />
                     </div>
+
+                    <div className="">
+                        <label htmlFor="uname">Электронная почта</label>{/*Я сделала расположение в кнопок не как в фигме, потому что такое мне кажется более логичным */}
+                        <Input id="uname" type="text" placeholder="you@example.com" /> {/*На страничке логина ув примере была почта, поэтому я ей дала айдишку uname, но, предполагаю, она должна быть у логина*/}
+                    </div>
+
                     <div className="">
                         <label htmlFor="password">Пароль</label>
                         <Input id="password" type="password" placeholder="••••••••" />
                     </div>
+
+                        <div className="">
+                        <label htmlFor="confirmpassword">Повторите пароль</label>
+                        <Input id="confirmpassword" type="password" placeholder="••••••••" />
+                    </div>
                 </form>
             </CardContent>
+
             <CardFooter className="flex flex-col">
-                <Button type="submit" className="w-full">Войти</Button>
+                <Button type="submit" className="w-full">Зарегистрироваться</Button>
+
                 <Button variant="link">
-                    <a href="./pages/Auth/RegistrationPage">
-                        <i>Нет аккаунта?</i>
+                    <a href="/login">
+                        <i>Уже есть аккаунт?</i>
                     </a>
                 </Button>
             </CardFooter>
         </Card>
     </div>
-    </>
 )
 }
 
