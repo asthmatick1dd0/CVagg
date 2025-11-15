14149da7fa4846c78547094e0fff921f5366d1e9
lobzikk <doctor.who-daleks@yandex.ru>
2025-11-15 21:04:19 +0300

    Implemented all of the auth features except the restoring password one

--- backend/internal/handlers/auth.go
+++ backend/internal/handlers/auth.go
@@ -1,15 +1,21 @@
 package handlers
 
 import (
+	"crypto/ecdsa"
+	"time"
+
 	"github.com/asthmatick1dd0/CVagg/internal/models"
 	"github.com/asthmatick1dd0/CVagg/internal/repository"
-	"github.com/asthmatick1dd0/CVagg/internal/transport"
+	"github.com/asthmatick1dd0/CVagg/internal/transport/dto"
+	"github.com/asthmatick1dd0/CVagg/internal/transport/input"
 	internal "github.com/asthmatick1dd0/CVagg/internal/validation"
 	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+	"golang.org/x/crypto/bcrypt"
 )
 
 func SignUpHandler(c *fiber.Ctx) error {
-	var input transport.SignUpInput
+	var input input.SignUpInput
 	err := c.QueryParser(&input)
 	if err != nil {
 		return c.Status(fiber.StatusBadRequest).JSON(err.Error())
@@ -22,10 +28,78 @@ func SignUpHandler(c *fiber.Ctx) error {
 	var user models.User
 	user.Email = input.Email
 	user.Username = input.Username
-	//TODO: Прохешировать пароль перед тем как ложить его куда-либо
-	user.PasswordHash = input.Password
+	//TODO: Добавить в валидацию требование чтобы пароль состоял тока из ASCII символов и был длиной <= 72 символа
+	//TODO: Придумать какой-то более надёжный ключ, чем bcrypt.DefaultCost
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(input.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(err)
+	}
+	user.PasswordHash = string(passwordHash)
 	if err := repo.Create(&user); err != nil {
 		return c.Status(fiber.StatusInsufficientStorage).JSON(err)
 	}
 	return c.Status(fiber.StatusAccepted).JSON(input)
+}
+
+func SignInHandler(c *fiber.Ctx) error {
+	var input input.SignInInputEmail
+	if err := c.QueryParser(&input); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(err.Error())
+	}
+	errs := internal.ValidateStruct(input)
+	if len(errs) != 0 {
+		return c.Status(fiber.StatusBadRequest).JSON(errs)
+	}
+	repo := c.Locals("userRepo").(repository.UserRepository)
+	ok, err := repo.ExistsByEmail(input.Email)
+	if err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON("wrong email or password")
+	}
+	if !ok {
+		return c.Status(fiber.StatusUnauthorized).JSON("wrong email or password")
+	}
+	user, err := repo.GetByEmail(input.Email)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(err)
+	}
+	if err := bcrypt.CompareHashAndPassword([]byte(user.PasswordHash), []byte(input.Password)); err != nil {
+		return c.Status(fiber.StatusUnauthorized).JSON(err)
+	}
+
+	token := jwt.New(jwt.SigningMethodES256)
+	now := time.Now().UTC()
+	claims := token.Claims.(jwt.MapClaims)
+	claims["sub"] = user.ID
+	claims["iat"] = now
+	claims["nbf"] = now
+	claims["exp"] = now.Add(c.Locals("JWTExpirationTime").(time.Duration))
+	_, err = token.SignedString(c.Locals("JWTSecret").(ecdsa.PrivateKey))
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(err.Error())
+	}
+	// c.Cookie(&fiber.Cookie{
+	// 	Name:     "token",
+	// 	Value:    tokenString,
+	// 	Path:     "/",
+	// 	MaxAge:   c.Locals("JWTMaxAge").(int),
+	// 	Secure:   false,
+	// 	HTTPOnly: true,
+	// 	Domain:   "localhost",
+	// })
+
+	return c.Status(fiber.StatusAccepted).JSON(input)
+}
+
+func LogOutHandler(c *fiber.Ctx) error {
+	c.Cookie(&fiber.Cookie{
+		Name:    "token",
+		Value:   "",
+		Expires: time.Now().Add(-time.Hour), //небольшая костылизация - возвращается просроченный токен
+	})
+	return c.SendString("Succesfully logged out")
+}
+
+func MeHandler(c *fiber.Ctx) error {
+	user := c.Locals("user").(dto.UserResponse)
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{"data": fiber.Map{"user": user}})
 }
--- backend/internal/routes/auth.go
+++ backend/internal/routes/auth.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"github.com/asthmatick1dd0/CVagg/internal/handlers"
+	"github.com/asthmatick1dd0/CVagg/internal/service"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -12,15 +13,11 @@ func AuthRoutes(app *fiber.App) {
 	// /api/v1/auth/....
 	auth := v1.Group("/auth")
 
-	auth.Get("/signin", func(c *fiber.Ctx) error {
-		return c.SendString("login route")
-	})
+	auth.Get("/signin", handlers.SignInHandler)
 
 	auth.Post("/signup", handlers.SignUpHandler)
 
-	auth.Post("/logout", func(c *fiber.Ctx) error {
-		return c.SendString("logout route")
-	})
+	auth.Post("/logout", service.DeserealizeUser, handlers.LogOutHandler)
 
 	auth.Post("/forgot-password", func(c *fiber.Ctx) error {
 		return c.SendString("forgot-password route")
@@ -30,7 +27,5 @@ func AuthRoutes(app *fiber.App) {
 		return c.SendString("refresh route")
 	})
 
-	auth.Get("/me", func(c *fiber.Ctx) error {
-		return c.SendString("me route")
-	})
+	auth.Get("/me", service.DeserealizeUser, handlers.MeHandler)
 }
