f2b4ef139f2272b9ba97b5cf1c86ddf502edbeee
Kekzuke <artem.golub2019@gmail.com>
2025-11-18 03:12:32 +0300

    add few more repos, services and handlers

--- backend/internal/container/handler_container.go
+++ backend/internal/container/handler_container.go
@@ -12,10 +12,15 @@ import (
 // Пополнять по мере подключения новых хендлеров к рутам
 type HandlerContainer struct {
 	resumeRepo repository.ResumeRepository
+	authRepo   repository.UserRepository
 
 	resumeService service.ResumeService
+	authService   service.AuthService
+	editorService service.EditorService
 
 	ResumeHandler handlers.ProfileHandler
+	AuthHandler   handlers.AuthHandler
+	EditorHandler handlers.EditorHandler
 }
 
 func NewHandlerContainer() *HandlerContainer {
@@ -25,21 +30,33 @@ func NewHandlerContainer() *HandlerContainer {
 	}
 
 	_resumeRepo := repository.NewResumeRepository(db)
+	_authRepo := repository.NewUserRepository(db)
 
 	log.Printf("Connected to DB successfully")
 
 	_resumeService := service.NewResumeService(_resumeRepo)
+	_authService := service.NewAuthService(_authService)
+	_editorService := service.NewEditorService(_resumeRepo)
 
 	log.Printf("Initialized Resume Service")
 
 	_resumeHandler := handlers.NewProfileHandler(_resumeService)
+	_authHandler := handlers.NewAuthHandler(_resumeService)
+	_editorHandler := handlers.NewEditorHandler(_editorService)
 
 	log.Printf("Initialized Resume Handler")
 
 	container := &HandlerContainer{
-		ResumeRepo:    _resumeRepo,
-		ResumeService: _resumeService,
+		resumeRepo: _resumeRepo,
+		authRepo:   _authRepo,
+
+		resumeService: _resumeService,
+		authService:   _authService,
+		editorService: _editorService,
+
 		ResumeHandler: _resumeHandler,
+		AuthHandler:   _authHandler,
+		EditorHandler: _editorHandler,
 	}
 
 	log.Printf("Initialized handler container")
