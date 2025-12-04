82bb468f635d9b7585d8189c8fe24449368797d1
Rin <igrigorian@sfedu.ru>
2025-12-04 10:49:05 +0300

    update services

--- frontend/src/services/resumeService.ts
+++ frontend/src/services/resumeService.ts
@@ -15,7 +15,15 @@ export const resumeApi = {
         return response.data;
     },
     fetchResumeById: async (id: string, userID: number = 1): Promise<Resume> => {
-        const response = await api.get<Resume>(`/resumes/:${id}?user_id=${userID}`);
+        const response = await api.get<Resume>(`/dashboard/resumes/:${id}?user_id=${userID}`);
+        return response.data;
+    },
+    saveResume: async (data: Partial<Resume>, userID: number = 1): Promise<Resume> => {
+        const response = await api.post<Resume>(`/editor/save?user_id=${userID}`, data);
+        return response.data;
+    },
+    updateResume: async (id: string, data: Partial<Resume>, userID: number = 1): Promise<Resume> => {
+        const response = await api.patch<Resume>(`/editor/:${id}/save?user_id=${userID}`, data);
         return response.data;
     },
 };
