45b4280a4c4108842c6578f0c97ff07776ec2d45
Rin <igrigorian@sfedu.ru>
2026-03-21 22:49:06 +0300

    update ResumeContext

--- frontend/src/pages/EditorPage/components/forms/ExperienceForm.tsx
+++ frontend/src/pages/EditorPage/components/forms/ExperienceForm.tsx
@@ -5,7 +5,6 @@ import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 import { Label } from "@/components/ui/label"
 import { Switch } from "@/components/ui/switch"
-import { Textarea } from "@/components/ui/textarea"
 import { 
   Select, 
   SelectContent, 
