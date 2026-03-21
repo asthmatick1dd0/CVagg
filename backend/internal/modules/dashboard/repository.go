6b7978c5cddc1dc4c2ce33ba9048ead838a51308
Kekzuke <artem.golub2019@gmail.com>
2026-03-21 18:38:01 +0300

    add transactions for each crud function

--- backend/internal/modules/dashboard/repository.go
+++ backend/internal/modules/dashboard/repository.go
@@ -2,15 +2,16 @@ package dashboard
 
 import (
 	"github.com/asthmatick1dd0/CVagg/internal/models"
+	"github.com/asthmatick1dd0/CVagg/pkg/helpers/cvaggerr"
 	"github.com/asthmatick1dd0/CVagg/pkg/helpers/cvagglog"
 	"gorm.io/gorm"
 )
 
 type Repository interface {
-	Create(user *models.Resume) error
+	Create(tx *gorm.DB, user *models.Resume) error
 	GetByID(id uint) (*models.Resume, error)
-	Update(resume *models.Resume) error
-	Delete(id uint) error
+	Update(tx *gorm.DB, resume *models.Resume) error
+	Delete(tx *gorm.DB, id uint) error
 	GetAllByUserID(userID uint) ([]*models.Resume, error)
 }
 
@@ -26,30 +27,50 @@ func NewRepository(db *gorm.DB, logger *cvagglog.Logger) Repository {
 	}
 }
 
-func (r *resumeRepo) Create(user *models.Resume) error {
-	return r.db.Create(user).Error
+func (r *resumeRepo) Create(tx *gorm.DB, user *models.Resume) error {
+	db := r.getDB(tx)
+	if err := db.Table("resumes.profile").Create(user).Error; err != nil {
+		return cvaggerr.ErrorDataBase()
+	}
+	return nil
 }
 
 func (r *resumeRepo) GetByID(id uint) (*models.Resume, error) {
 	var resume models.Resume
-	if err := r.db.First(&resume, id).Error; err != nil {
-		return nil, err
+	if err := r.db.Table("resumes.profile").First(&resume, id).Error; err != nil {
+		return nil, cvaggerr.ErrorDataBase()
 	}
 	return &resume, nil
 }
 
-func (r *resumeRepo) Update(resume *models.Resume) error {
-	return r.db.Save(resume).Error
+func (r *resumeRepo) Update(tx *gorm.DB, resume *models.Resume) error {
+	db := r.getDB(tx)
+	if err := db.Table("resumes.profile").Save(resume).Error; err != nil {
+		return cvaggerr.ErrorDataBase()
+	}
+	return nil
 }
 
-func (r *resumeRepo) Delete(id uint) error {
-	return r.db.Where("id = ?", id).Delete(&models.Resume{}).Error
+func (r *resumeRepo) Delete(tx *gorm.DB, id uint) error {
+	var entity models.Resume
+	db := r.getDB(tx)
+	if err := db.Table("resumes.profile").Delete(&entity, id).Error; err != nil {
+		return cvaggerr.ErrorDataBase()
+	}
+	return nil
 }
 
 func (r *resumeRepo) GetAllByUserID(userId uint) ([]*models.Resume, error) {
 	var resumes []*models.Resume
-	if err := r.db.Where("user_id = ?", userId).Find(&resumes).Error; err != nil {
-		return nil, err
+	if err := r.db.Table("resumes.profile").Where("user_id = ?", userId).Find(&resumes).Error; err != nil {
+		return nil, cvaggerr.ErrorDataBase()
 	}
 	return resumes, nil
+}
+
+func (r *resumeRepo) getDB(tx *gorm.DB) *gorm.DB {
+	if tx != nil {
+		return tx
+	}
+	return r.db
 }
