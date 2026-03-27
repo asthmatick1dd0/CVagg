7031b879527f94a5f40907b87d4e7c1a71b646ef
Rin <igrigorian@sfedu.ru>
2026-03-27 08:31:54 +0300

    add tabs to editor

--- frontend/src/pages/EditorPage/EditorPage.tsx
+++ frontend/src/pages/EditorPage/EditorPage.tsx
@@ -5,6 +5,7 @@ import { ResumeProvider, useResumeContext } from "@/contexts/ResumeContext";
 import { PDFViewer } from "@react-pdf/renderer";
 import ResumeDocument from "@/components/pdf/ResumeDocument";
 import { useDebounce } from "@uidotdev/usehooks";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 
 const ResumePreview = () => {
   const { resumeData, loading } = useResumeContext();
@@ -33,7 +34,18 @@ const EditorContent = () => {
         <EditorInputs />
         <div className="bg-primary/60 rounded-xl p-6 min-h-[400px] max-md:hidden">
           <div className="w-full h-full object-cover rounded-lg">
-            <ResumePreview />
+            <Tabs defaultValue="preview">
+            <TabsList>
+              <TabsTrigger value="preview">Превью</TabsTrigger>
+              <TabsTrigger value="chat">Чат</TabsTrigger>
+            </TabsList>
+            <TabsContent value="preview">
+              <ResumePreview />
+            </TabsContent>
+            <TabsContent value="chat">
+              <ResumePreview />
+            </TabsContent>
+          </Tabs>
           </div>
         </div>
       </div>
