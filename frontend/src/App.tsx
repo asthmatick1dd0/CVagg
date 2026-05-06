718a4696b578434ef2b8d1e51144a01e2ac43579
smokinblackdog <igrigorian@sfedu.ru>
2026-05-06 14:37:05 +0300

    fix: app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom"
-// import LandingPage from "./pages/LandingPage/LandingPage"
+import LandingPage from "./pages/LandingPage/LandingPage"
 import DashboardPage from "./pages/Dashboard/DashboardPage";
 import LoginPage from "./pages/Auth/LoginPage";
 import RegistrationPage from "./pages/Auth/RegistrationPage";
@@ -13,22 +13,32 @@ function App() {
 
   return (
     <>
-      <ResumeProvider>
-        <Routes>
-          <Route path="/" element={<DashboardPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/registration" element={<RegistrationPage />} />
-          <Route
-            path="/dashboard"
-            element={user ? <DashboardPage /> : <Navigate to="/login" replace />}
-          />
-          <Route 
-          path="/editor/:id" 
-          element={user ? <EditorPage /> : <Navigate to="/login" replace />} 
-          /> 
-        </Routes>
-      </ResumeProvider>
-      <ThemeToggle/>
+    <ResumeProvider>
+      <Routes>
+        <Route path="/" element={<LandingPage/>} />
+        <Route
+        path="/login"
+        element={user ? <Navigate to="/dashboard" replace /> : <LoginPage />}
+        />
+        <Route
+          path="/registration"
+          element={user ? <Navigate to="/login" replace /> : <RegistrationPage />}
+        />
+        <Route
+          path="/dashboard"
+          element={user ? <DashboardPage /> : <Navigate to="/login" replace />}
+        />
+        <Route 
+        path="/editor/:id" 
+        element={user ? <EditorPage /> : <Navigate to="/login" replace />} 
+        /> 
+        <Route 
+        path="/" 
+        element={<EditorPage />} 
+        />
+      </Routes>
+    </ResumeProvider>
+    <ThemeToggle/>
     </>
   );
 }
