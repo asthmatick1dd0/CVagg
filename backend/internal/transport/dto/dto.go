bc657941645de9749e0242c1b01d5bce68196249
Kekzuke <artem.golub2019@gmail.com>
2026-05-06 16:24:18 +0300

    add new fields(name, surname)

--- backend/internal/transport/dto/dto.go
+++ backend/internal/transport/dto/dto.go
@@ -8,6 +8,8 @@ import (
 type UserResponse struct { // это пихать на фронт, валидировать и хранить локально в памяти для каждого юзверя (но на самом деле можно и по беку рассылать)
 	ID        uint      `json:"id,omitempty"`
 	Username  string    `json:"name,omitempty"`
+	Name      string    `json:"name,omitempty"`
+	Surname   string    `json:"surname,omitempty"`
 	Email     string    `json:"email,omitempty"`
 	ExpiresAt time.Time `json:"expires_at"`
 	UpdatedAt time.Time `json:"updated_at"`
