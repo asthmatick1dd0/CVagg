21356e4d4d8ba9f73973092ab1835501cea8822b
Andrew Khrobostov <andrew.khrobostov@gmail.com>
2026-05-08 09:40:37 +0300

    Refactor Avatar field in PersonalDataInput to use pointer type and update service methods accordingly

--- backend/internal/modules/editor/service.go
+++ backend/internal/modules/editor/service.go
@@ -284,7 +284,7 @@ func (s *service) UpdatePersonalData(tx *gorm.DB, it *input.ItemInput, ID uint) 
 		Website:    &it.PersonalData.Website,
 		Github:     &it.PersonalData.Github,
 		BirthDate:  &it.PersonalData.Birthdate,
-		Avatar:     &it.PersonalData.Avatar,
+		Avatar:     it.PersonalData.Avatar,
 	}
 
 	if err := s.personalDataRepo.Update(tx, personalDataModel, "resume_item.personal_data", it.FieldID); err != nil {
@@ -425,7 +425,7 @@ func (s *service) SavePersonalData(tx *gorm.DB, it *input.ItemInput, ID uint) cv
 		Website:    &it.PersonalData.Website,
 		Github:     &it.PersonalData.Github,
 		BirthDate:  &it.PersonalData.Birthdate,
-		Avatar:     &it.PersonalData.Avatar,
+		Avatar:     it.PersonalData.Avatar,
 	}
 
 	if err := s.personalDataRepo.Create(tx, personalDataModel, "resume_item.personal_data"); err != nil {
--- backend/internal/transport/input/resume_input.go
+++ backend/internal/transport/input/resume_input.go
@@ -30,7 +30,7 @@ type PersonalDataInput struct {
 	Github     string    `json:"github"`
 	Website    string    `json:"website"`
 	Birthdate  time.Time `json:"birthdate"`
-	Avatar     string    `json:"avatar"`
+	Avatar     *string   `json:"avatar,omitempty"`
 }
 
 type EducationInput struct {
